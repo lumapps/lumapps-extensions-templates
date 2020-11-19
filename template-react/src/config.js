19b7cf47c0ec4ddef8e1344a627a1c947270ecdd
Grégory CAT <gregory@lumapps.com>
2020-11-19 14:11:40 +0100

    feat(config): update config files for extensions
    Plus update deps in packages.json

--- template-react/src/config.js
+++ template-react/src/config.js
@@ -1,54 +1,63 @@
-  
 /**
  *  Create-LumApps-Widget config file
  *  update the following to fit your needs
  */
+
 export const useGlobalSettings = true;
 
+/**
+ * The ids of your provider and extension
+ */
 const providerId = 'LumAppsLAB';
+const extensionId = '';
 
-const bundleName = 'SampleWidget';
 
 const description = {
-  en: 'SampleWidget',
+    en: 'SampleWidgetTypescript',
 };
 
 const name = {
-  en: 'SampleWidget',
+    en: 'SampleWidgetTypescript',
 };
 
-const icon = 'https://lh3.googleusercontent.com/-6F_iwel8KGY/WD8MdGS0PkI/AAAAAAAAGDI/KaqvKtZCK_AsdK6BcKp6Cy1MpwoEWCUvgCKgB/s400/logo.png'; // a working link to your widget icon
+const icon = {
+    en: 'https://lh3.googleusercontent.com/-6F_iwel8KGY/WD8MdGS0PkI/AAAAAAAAGDI/KaqvKtZCK_AsdK6BcKp6Cy1MpwoEWCUvgCKgB/s400/logo.png', // a working link to your widget icon
+};
+
+/**
+ * The components available for your extensions
+ * 'content' : For the Widget content itself (required)
+ * 'settings' : For your widget settings
+ * 'globalSettings' : For globalsettings used by platform admin.
+ */
+const components = ['content', 'settings', 'global_settings'];
+
+// Whether the extension is public or not in the marketplace.
+const isPublic = true;
+
+/**
+ * The list of authorized customer ids.
+ * 
+ * If your extension is not public only these customers will see and 
+ * will be able to install this extensions.
+ */
+const whitelist = [''];
 
 // do not change the following unless you know what you are doing
 const config = {
-  bundleName: `${bundleName}.mdr`,
-  description,
-  name,
-  extensionComponents: [
-    {
-      componentName: 'Widget', // must reflect the component exported in 'index.widget.js' and/or index.widget.no_global.js
-      type: 'WidgetContent', // DO NOT CHANGE
-      version: '0.0.1',
-    },
-    {
-      componentName: 'WidgetSettings', // must reflect the component exported in 'index.widget.js' and/or index.widget.no_global.js
-      type: 'WidgetSettings', // DO NOT CHANGE
-      version: '0.0.1',
-    },
-    {
-      componentName: 'WidgetGlobalSettings', // must reflect the component exported in 'index.widget.js' and/or index.widget.no_global.js
-      type: 'GlobalWidgetSettings', // DO NOT CHANGE
-      version: '0.0.1',
-    },
-  ],
-  icon,
-  providerId,
-  type: 'widget',
-  isActivated: true,
+    providerId,
+    extensionId,
+    name,
+    description,
+    icon,
+    components,
+    isPublic,
+    whitelist,
+    category: 'widget',
 };
 
 if (!useGlobalSettings) {
-  config.extensionComponents.pop();
+    config.components.pop();
 }
 
 export default config;
