ba0c67dd4b724ce6f7eeacee2f6e162ad0a28dcf
Gregory CAT <gregory@lumapps.com>
2021-04-06 16:50:18 +0200

    fix(index): import store from playground

--- template-react-ts/src/index.tsx
+++ template-react-ts/src/index.tsx
@@ -1,4 +1,4 @@
-import { Playground } from '@lumapps-extensions-playground/devenv';
+import { Playground, store } from '@lumapps-extensions-playground/devenv';
 
 import '@lumapps-extensions-playground/devenv/devenv.esm.css';
 
@@ -7,17 +7,20 @@ import '@lumx/core/lumx.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import config from './config.js';
 import { Widget, WidgetGlobalSettings, WidgetSettings } from './widget';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Playground
-            config={config}
-            Widget={Widget}
-            WidgetSettings={WidgetSettings}
-            WidgetGlobalSettings={WidgetGlobalSettings}
-        />
+        <Provider store={store}>
+            <Playground
+                config={config}
+                Widget={Widget}
+                WidgetSettings={WidgetSettings}
+                WidgetGlobalSettings={WidgetGlobalSettings}
+            />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
