dac58cc9bd81bc49ebc1d7980de41fcb54eb2a0b
Thomas VANDERMEERSCH <thomas.vandermeersch@lumapps.com>
2022-06-09 15:59:25 +0200

    feat(search-extension-template): add types
    DIS-350

--- template-search-extension/src/widget/Search/index.ts
+++ template-search-extension/src/widget/Search/index.ts
@@ -1,3 +1,11 @@
-export const searchFunction = () => {
+import { ExtensionInput, ExtensionOutput, LOADING_STATUS } from '../../types';
+
+export const searchFunction = async ({ query, page, pageSize }: ExtensionInput): Promise<ExtensionOutput> => {
     // Search functionnality ....
+    return {
+        results: [],
+        totalCount: 0,
+        hasMore: false,
+        status: LOADING_STATUS.FETCHED,
+    }
 }
